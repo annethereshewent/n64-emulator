e8d01bb8d4b492217b744d7ec86b71d75493fc8f
Anne Castrillon <anne.castrillon@gmail.com>
2025-04-04 15:12:11 -0700

    got it sending commands to swift app!

--- src/bus/rdp/RDPInterface.cpp
+++ src/bus/rdp/RDPInterface.cpp
@@ -1,6 +1,9 @@
 #include "RDPInterface.hpp"
 #include <cstdint>
 #include <iostream>
+#include <bit>
+#include <vector>
+#include <array>
 #include "../../config.hpp"
 #ifdef USING_SDL3
     #include "../../parallel-rdp-files/interface.cpp"
@@ -52,16 +55,19 @@ void RDPInterface::writeRegisters(uint32_t offset, uint32_t value) {
             if (((status >> Freeze) & 0b1) == 0) {
                 #ifdef USING_SDL3
                     uint64_t cycles = rdp_process_commands();
-                    pipeBusy = 0xffffff;
-
-                    setBit(&status, Gclk);
-                    setBit(&status, CmdBusy);
-                    setBit(&status, PipeBusy);
-
-                    if (cycles > 0) {
-                        bus.cpu.scheduler.addEvent(Event(RDPEvent, bus.cpu.cop0.count + cycles));
-                    }
+                #else
+                    uint64_t cycles = processRdpCommands();
                 #endif
+
+                pipeBusy = 0xffffff;
+
+                setBit(&status, Gclk);
+                setBit(&status, CmdBusy);
+                setBit(&status, PipeBusy);
+
+                if (cycles > 0) {
+                    bus.cpu.scheduler.addEvent(Event(RDPEvent, bus.cpu.cop0.count + cycles));
+                }
             } else {
                 isFrozen = true;
             }
@@ -117,3 +123,112 @@ void RDPInterface::updateStatus(uint32_t value) {
         clockCounter = 0;
     }
 }
+
+#ifndef USING_SDL3
+    uint64_t RDPInterface::processRdpCommands() {
+        std::println("processing RDP commands");
+        uint32_t cycles = 0;
+        int length = end - current;
+
+        if (length <= 0) {
+            return cycles;
+        }
+
+        uint32_t uLength = (uint32_t)length  >> 3;
+
+        if ((cmdPtr + uLength) & ~(0x0003FFFF >> 3)) {
+            return cycles;
+        }
+
+        uint32_t offset = current;
+
+        if (getBit(status, Xbus)) {
+            do {
+                offset &= 0xff8;
+
+                cmdBuffer[2 * cmdPtr] = std::byteswap(*(uint32_t*)&bus.rsp.dmem[offset]);
+                cmdBuffer[2 * cmdPtr + 1] = std::byteswap(*(uint32_t*)&bus.rsp.dmem[offset + 4]);
+                offset += sizeof(uint64_t);
+                cmdPtr++;
+            } while (--length > 0);
+        } else {
+            do {
+                offset &= 0xFFFFF8;
+
+                cmdBuffer[2 * cmdPtr] = std::byteswap(*(uint32_t*)&bus.rdram[offset]);
+                cmdBuffer[2 * cmdPtr + 1] = std::byteswap(*(uint32_t*)&bus.rdram[offset + 4]);
+                offset += sizeof(uint64_t);
+                cmdPtr++;
+            } while (--length > 0);
+        }
+
+        while (currCmd - cmdPtr < 0)
+        {
+            uint32_t word1 = cmdBuffer[2 * currCmd];
+            uint32_t word2 = cmdBuffer[2 * currCmd + 1];
+
+            uint32_t command = (word1 >> 24) & 63;
+
+            int cmdLength = cmdLengths[command];
+
+            if (cmdPtr - currCmd - cmdLength < 0)
+            {
+                start = current = end;
+
+                if (enqueuedCommands.size() > 0) {
+                    cmdsReady = true;
+                }
+
+                return cycles;
+            }
+
+            if (command >= 8) {
+                enqueuedCommands.push_back(command);
+                enqueueCommandWords(cmdLength * 2, &cmdBuffer[2 * currCmd]);
+            }
+
+            if (RdpOp(command) == RdpOp::SetScissor)
+            {
+                uint32_t upperLeftX = ((word1 >> 12) & 0xFFF) >> 2;
+                uint32_t upperLeftY = (word1 & 0xFFF) >> 2;
+                uint32_t lowerRightX = ((word2 >> 12) & 0xFFF) >> 2;
+                uint32_t lowerRightY = (word2 & 0xFFF) >> 2;
+                region = (lowerRightX - upperLeftX) * (lowerRightY - upperLeftY);
+            }
+            if (RdpOp(command) == RdpOp::SyncFull)
+            {
+                cycles = region;
+
+                if (cycles == 0) {
+                    cycles = 5000;
+                }
+            }
+
+            currCmd += cmdLength;
+        }
+
+        cmdPtr = 0;
+        currCmd = 0;
+        current = end;
+
+        if (enqueuedCommands.size() > 0) {
+            cmdsReady = true;
+        }
+
+        return cycles;
+    }
+
+    void RDPInterface::enqueueCommandWords(int numWords, uint32_t* words) {
+        std::vector<uint32_t> result = {};
+        for (int i = 0; i < numWords; i++) {
+            result.push_back(words[i]);
+        }
+
+        enqueuedWords.push_back(result);
+    }
+
+    void RDPInterface::clearCommands() {
+        enqueuedWords.clear();
+        enqueuedCommands.clear();
+    }
+#endif
--- src/bus/rdp/RDPInterface.hpp
+++ src/bus/rdp/RDPInterface.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
+#include <array>
+#include "../../config.hpp"
 
 class Bus;
 
@@ -18,6 +21,119 @@ enum DPCStatusBits {
     StartPending = 10
 };
 
+#ifndef USING_SDL3
+    const unsigned cmdLengths[64] = {
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        4,
+        6,
+        12,
+        14,
+        12,
+        14,
+        20,
+        22,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        2,
+        2,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+    };
+    enum class RdpOp {
+        Nop = 0,
+
+        MetaSignalTimeline = 1,
+        MetaFlush = 2,
+        MetaIdle = 3,
+        MetaSetQuirks = 4,
+
+        FillTriangle = 0x08,
+        FillZBufferTriangle = 0x09,
+        TextureTriangle = 0x0a,
+        TextureZBufferTriangle = 0x0b,
+        ShadeTriangle = 0x0c,
+        ShadeZBufferTriangle = 0x0d,
+        ShadeTextureTriangle = 0x0e,
+        ShadeTextureZBufferTriangle = 0x0f,
+        TextureRectangle = 0x24,
+        TextureRectangleFlip = 0x25,
+        SyncLoad = 0x26,
+        SyncPipe = 0x27,
+        SyncTile = 0x28,
+        SyncFull = 0x29,
+        SetKeyGB = 0x2a,
+        SetKeyR = 0x2b,
+        SetConvert = 0x2c,
+        SetScissor = 0x2d,
+        SetPrimDepth = 0x2e,
+        SetOtherModes = 0x2f,
+        LoadTLut = 0x30,
+        SetTileSize = 0x32,
+        LoadBlock = 0x33,
+        LoadTile = 0x34,
+        SetTile = 0x35,
+        FillRectangle = 0x36,
+        SetFillColor = 0x37,
+        SetFogColor = 0x38,
+        SetBlendColor = 0x39,
+        SetPrimColor = 0x3a,
+        SetEnvColor = 0x3b,
+        SetCombine = 0x3c,
+        SetTextureImage = 0x3d,
+        SetMaskImage = 0x3e,
+        SetColorImage = 0x3f
+    };
+#endif
+
 class RDPInterface {
 public:
     uint32_t status = 0;
@@ -28,6 +144,18 @@ public:
 
     uint32_t pipeBusy = 0xffffff;
 
+    #ifndef USING_SDL3
+        std::array<uint32_t, 0x10000> cmdBuffer = {};
+        int currCmd = 0;
+        int cmdPtr = 0;
+        uint32_t region = 0;
+
+        bool cmdsReady = false;
+
+        std::vector<uint32_t> enqueuedCommands = {};
+        std::vector<std::vector<uint32_t>> enqueuedWords = {};
+    #endif
+
     uint32_t clockCounter = 0;
     bool isFrozen = false;
     bool frameFinished = false;
@@ -35,6 +163,13 @@ public:
     RDPInterface(Bus& bus): bus(bus) {};
 
     Bus& bus;
+
+    #ifndef USING_SDL3
+        uint64_t processRdpCommands();
+        void enqueueCommandWords(int numWords, uint32_t* words);
+
+        void clearCommands();
+    #endif
 
     uint32_t readRegisters(uint32_t offset);
     void writeRegisters(uint32_t offset, uint32_t value);
